04c6f035e8bcbb7f2054e2e5912e7fb38b717622
Proggit <proggit@mail.ru>
2021-04-04 11:44:01 +0500

    2 step

--- src/gendiffHelp.js
+++ src/gendiffHelp.js
@@ -3,9 +3,11 @@ import { Command } from 'commander';
 const program = new Command();
 
 program
+  .arguments('<filepath1> <filepath2>')
   .description('Usage: gendiff [options]')
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0', '-V, --version', 'output the version number')
-  .helpOption('-h, --help', 'output usage information');
+  .helpOption('-h, --help', 'output usage information')
+  .option('-f, --format [type]', 'output format');
 
 export default program;
