20625e41f000484fb2903220f8081f48460d5459
Proggit <proggit@mail.ru>
2021-05-02 18:29:37 +0500

    step 8

--- src/run.js
+++ src/run.js
@@ -1,7 +1,7 @@
-import { Command } from 'commander';
+import commander from 'commander';
 import genDiff from './index.js';
 
-const program = new Command();
+const program = new commander;
 
 program
   .arguments('<filepath1> <filepath2>')
